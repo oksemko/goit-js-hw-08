9966a002f366abd0615546303a984cb283f5e2e4
Oksana Semko <osemko@gmail.com>
2022-06-17 19:20:00 +0300

    task 01_solution

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const KEY = 'feedback-form-state';
+const formData = {};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+updateForm();
+
+function updateForm() {
+  const savedData = localStorage.getItem(KEY);
+  if (savedData) {
+    const { email, message } = JSON.parse(savedData);
+    form.email.value = email;
+    form.message.value = message;
+    formData.email = email;
+    formData.message = message;
+  }
+}
+
+function onFormInput(event) {
+  formData.email = form.elements.email.value;
+  formData.message = form.elements.message.value;
+  localStorage.setItem(KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formDataToSend = new FormData(event.currentTarget);
+  formDataToSend.forEach((value, name) => {
+    formData[name] = value;
+  });
+
+  event.currentTarget.reset();
+  localStorage.removeItem(KEY);
+
+  console.log(formData);
+}
